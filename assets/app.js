5f4fdba73f83801e4897e497b6acd72a8c9d217f
Emilie <emilie.b.holmberg@hotmail.dk>
2025-03-29 11:10:02 +0100

    home page

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
-import './scripts/home.js';
-
-import './styles/app.css';
+import './scripts/home.js'; 
+ 
+import './styles/app.css'; //CSS styles for the base app parts
+import './styles/home.css'; //CSS styles for home page
