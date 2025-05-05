690a8131f68273a7bbe5f5a06cca8a8b63ae9e9d
Ramla17 <142784433+Ramla17@users.noreply.github.com>
2025-05-05 15:56:11 +0200

    fjernet twilio-nøgle

--- src/Form/SmsService.php
+++ src/Form/SmsService.php
@@ -12,10 +12,11 @@ class SmsService
 
     public function __construct()
     {   
-        // Denne information kan findes i bunden af twilio hjemmesiden
-        $this->twilioSid = 'AC261da035ec6da6e8f4b85b504ea1c0e2';    
-        $this->twilioToken = '3e41867063140646f61586d768c5f0f4';  
-        $this->twilioFrom = '+19787055940'; 
+        // Denne information kan findes i bunden af twilio hjemmesiden.
+        // MIDLERTIDIGT FJERNET! JEG SÆTTER IND IGEN SENERE
+        $this->twilioSid = '0';    
+        $this->twilioToken = '0';  
+        $this->twilioFrom = '0'; 
     }
 
     public function sendSms(string $to, string $message): void
